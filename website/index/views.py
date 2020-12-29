a52f0ff0ea9e0fcd0d8d96ae9d89c57faff39883
JianWu <jian_dfh@hotmail.com>
2020-12-29 20:02:36 +0100

    changed pictures

--- website/index/views.py
+++ website/index/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, template_name='mainContent.html')
+
+def hdnytorv(request):
+    print('here')
+    return render(request, template_name='hdnytorv.html')
     
--- website/website/urls.py
+++ website/website/urls.py
@@ -20,4 +20,5 @@ from index import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name = 'index'),
+    path('hdnytorv', views.hdnytorv, name='hdnytorv'),
 ]
