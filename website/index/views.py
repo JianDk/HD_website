cca6ab2c9023970f4ebbcc07455a7145777130ff
JianWu <jian_dfh@hotmail.com>
2020-12-31 08:16:20 +0100

    rearranged the mainContent into multiple files for easier management

--- website/index/views.py
+++ website/index/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, template_name='mainContent.html')
+    return render(request, template_name='index.html')
 
 def hdnytorv(request):
     print('here')
