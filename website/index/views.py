3ee085be00aa8965bab0b8be9e71b48d197750e6
JianWu <jian_dfh@hotmail.com>
2021-02-18 22:24:38 +0100

    working contact form

--- website/index/views.py
+++ website/index/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
 from .forms import contactForm
 
 # Create your views here.
@@ -9,12 +10,15 @@ def hdnytorv(request):
     return render(request, template_name='hdnytorv.html')
 
 def hd2900(request):
-    print('request starts here')
-    print(request)
-    print('request ends here')
-    form = contactForm()
-    context = {"form" : form,
-    "emailStatus" : "hello world"}
-    return render(request, template_name = 'hd2900.html', context = context)
+
+    if request.method == 'POST':
+        form = contactForm(request.POST)
+        if form.is_valid():
+            messages.success(request, 'Message sent')
+            return redirect('/hd2900#contactField')
+    else:      
+        form = contactForm()
+        context = {"form" : form}
+        return render(request, template_name = 'hd2900.html', context = context)
 
     
