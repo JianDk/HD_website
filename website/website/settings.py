a5a98e043ada2645a8c276047702951d42507dcd
Jian Wu <jian_dfh@hotmail.com>
2021-03-13 21:59:46 +0000

    production version without recaptcha

--- website/website/settings.py
+++ website/website/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = data['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['139.162.163.35']
 
