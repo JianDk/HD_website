1818beb62ee14e13ba7edc657f9c70f1f1fa7a6d
Jian Wu <jian_dfh@hotmail.com>
2021-03-13 16:48:55 +0000

    first version online

--- website/website/settings.py
+++ website/website/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import json
+
+#Import secret key
+with open('/etc/config.json','r') as fileId:
+    data = json.load(fileId)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ikk&bpir0bb-b!vjwpp2!9f3pw4#9(hoaoq=3#lhw6q(fmy%m1'
+SECRET_KEY = data['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
