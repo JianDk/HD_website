2ef637129521eaac4f2eb0d59258cebcff68406c
JianWu <jian_dfh@hotmail.com>
2021-03-20 12:04:01 +0100

    cred path changed

--- website/website/Modules/recaptchaValidate.py
+++ website/website/Modules/recaptchaValidate.py
@@ -18,5 +18,3 @@ class Validate:
         req =  urllib.request.Request(url, data=data)
         response = urllib.request.urlopen(req)
         self.result = json.loads(response.read().decode())
-        print('here is the result')
-        print(self.result)
