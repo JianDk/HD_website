7643e411887c7cae92cd4075ad0e54085d859aab
JianWu <jian_dfh@hotmail.com>
2021-05-22 08:51:18 +0200

    added mod_wsgi.server back to settings.py

--- website/website/settings.py
+++ website/website/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+#'mod_wsgi.server',
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
