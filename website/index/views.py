44c65c7c3af6062094c08eb8f1c4d8d9e489595e
JianWu <jian_dfh@hotmail.com>
2021-06-14 19:41:54 +0200

    update

--- website/index/views.py
+++ website/index/views.py
@@ -117,7 +117,7 @@ class hdnytorv(View):
             ('MENU', '/hdnytorv#menuHeader',),
             ('ABOUT US', '/hdnytorv#aboutUsLogo',),
             ('CONTACT', '/hdnytorv#contactForm',),
-            ('BOOK TABLE', 'https://book.easytablebooking.com/book/?id=bb7ae&lang=en',)
+            ('BOOK TABLE', '/hdnytorv#easyTableBookingContainer',)
         ])
     
     def get(self, request, *args, **kwargs):
